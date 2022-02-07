02cda1721c9d37cb722c9ce376fc80554a416043
norm-edinmukinovic <97086405+norm-edinmukinovic@users.noreply.github.com>
2022-02-07 14:35:26 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../main.css">
+    <link rel="stylesheet" href="./assetst/fonts/typography.css">
+
+    <title>MIZZUNI</title>
+</head>
+<body>
+    
+<div class="nav">
+    <div class="nav__left overline">
+        <a href="">Living</a>
+        <a href="">Professional</a>
+        <a href="">Inspiration</a>
+        <a href="">Hilfe</a>
+    </div>
+
+    <div class="nav__logo">
+        <img src="./assetst/icons/Vector.svg" alt="">
+    </div>
+
+    <div class="nav__right overline">
+        <a href="">Anmelden</a>
+        <a href="">Geschäftskunden</a>
+        <img src="./assetst/icons/search.svg" alt="">
+        <img src="./assetst/icons/hart.svg" alt="">
+        <img src="./assetst/icons/shopping-cart-simple.svg" alt="">
+    </div>
+</div>

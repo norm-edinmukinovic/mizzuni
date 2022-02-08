02d5c56f7696418bb22e515efc17517ecec88e20
norm-edinmukinovic <edin.mukinovic@imel.ba>
2022-02-08 16:04:47 +0100

    updates

--- footer.php
+++ footer.php
@@ -1,2 +1,44 @@
+<div class="footer">
+
+    <div class="footer__newsletter">
+
+        <div class="footer__newsletter-info">
+            <div class="footer__newsletter-info__title headline_small">
+                Mizzuni Newsletter
+            </div>
+            <div class="footer__newsletter-info__text base-small_light">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
+            sed diam nonumy eirmod tempor invidunt ut labore.
+            </div>
+        </div>
+
+        <div class="footer__newsletter-email">
+            <input type="email" id="email" name="email" placeholder="Ihre Email">
+            <div class="footer__newsletter-email--text textXS_light">
+            Mit der Anmeldung stimmen Sie der <span>Datenschutzerklärung</span> zu.
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer__info">
+
+        <div class="footer__info-contact">
+            <div>
+                Hilfe und Kontakt
+            </div>
+        </div>
+
+        <div class="footer__info-links">
+
+        </div>
+
+    </div>
+
+
+
+
+</div>
+
 </body>
 </html>

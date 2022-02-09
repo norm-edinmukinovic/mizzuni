3a5f350939805dab41dbdfff0577cfbd6af66b17
norm-edinmukinovic <edin.mukinovic@imel.ba>
2022-02-09 15:08:01 +0100

    updates

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ include "./layouts/intro/intro.php";
 
 include "./layouts/gallery/gallery.php";
 
+include "./layouts/services/services.php";
+
 include "footer.php";
 ?>
---
+++ layouts/services/services.php
@@ -0,0 +1,65 @@
+<div class="services">
+    <div class="services__subtitle overline">
+        Services
+    </div>
+
+    <div class="services__title headline__medium">
+        Wir helfen Ihnen gerne weiter
+    </div>
+
+    <div class="services__grid">
+
+        <div class="services__grid-card">
+            <div class="services__grid-card--image">
+            <img src="../../assetst/images/card1.svg" alt="">
+
+            <div class="button_text">
+            Online-Beratung <img src="../../assetst/icons/arrow.svg" alt="">
+            </div>
+            </div>
+
+            <div class="services__grid-card--text base-small_light">
+            Weit hinten, hinter den Wortbergen, 
+            fern der Länder Vokalien und Konsonantien leben die Blindtexte.
+            </div>
+
+
+        </div>
+
+        <div class="services__grid-card">
+            <div class="services__grid-card--image">
+            <img src="../../assetst/images/card2.svg" alt="">
+
+            <div class="button_text">
+            Muster bestellen <img src="../../assetst/icons/arrow.svg" alt="">
+            </div>
+            </div>
+
+            <div class="services__grid-card--text base-small_light">
+            Ein kleines Bächlein namens Duden fließt durch ihren Ort 
+            und versorgt sie mit den nötigen Regelialien.
+            </div>
+
+        </div>
+
+        <div class="services__grid-card">
+            <div class="services__grid-card--image">
+            <img src="../../assetst/images/card3.svg" alt="">
+
+            <div class="button_text">
+            Produktberatung <img src="../../assetst/icons/arrow.svg" alt="">
+            </div>
+            </div>
+
+            <div class="services__grid-card--text base-small_light">
+            Es ist ein paradiesmatisches Land, in dem einem gebratene 
+            Satzteile in den Mund fliegen.
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</div>

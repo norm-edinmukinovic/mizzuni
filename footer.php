371419e79426f50b877141b468954b5bf73ed059
norm-edinmukinovic <edin.mukinovic@imel.ba>
2022-02-09 13:52:24 +0100

    updates

--- footer.php
+++ footer.php
@@ -24,13 +24,128 @@
     <div class="footer__info">
 
         <div class="footer__info-contact">
-            <div>
+            <div class="overline">
                 Hilfe und Kontakt
             </div>
+
+            <div class="footer__info-contact--title headline_small">
+            Wie können wir helfen?
+            </div>
+
+            <div class="footer__info-contact--text">
+                <div class="base_light">
+                    Mo - Fr, 9:00 - 18:00 
+                    <span class="textXS_medium"><img src="./assetst/icons/ellipse_green.svg" alt=""> jetzt erreichbar</span>
+                </div>
+
+                <div class="base_light">
+                    <img src="./assetst/icons/phone.svg" alt=""> +49 (0) 12 – 34 56 78 99
+                </div>
+
+                <div class="base_light">
+                    <img src="./assetst/icons/envelope.svg" alt=""> hallo@mizzuni.com
+                </div>
+
+            </div>
+
+            <div class="footer__info-contact--link headline_light">
+            <span>Häufig gestellte Fragen</span>  <img src="./assetst/icons/arrow.svg" alt="">
+            </div>
+
         </div>
 
         <div class="footer__info-links">
 
+            <div class="footer__info-links--mizzuni">
+
+                <div class="overline">
+                Mizzuni
+                </div>
+
+                <a class="base_light" href="#">Über uns</a>
+                <a class="base_light" href="#">Nachhaltigkeit</a>
+                <a class="base_light" href="#">Versandoptionen</a>
+                <a class="base_light" href="#">Muster bestellen</a>
+                <a class="base_light" href="#">Partner werden</a>
+            </div>
+
+            <div class="footer__info-links--geschaft">
+
+                <div class="overline">
+                Für Geschäftskunden
+                </div>
+
+                <a class="base_light" href="#">Hilfe & Kontakt</a>
+                <a class="base_light" href="#">Design-Beratung</a>
+                <a class="base_light" href="#">Projektplanung</a>
+                <a class="base_light" href="#">AGBs (B2B)</a>
+            </div>
+
+            <div class="footer__info-links--privat">
+
+                <div class="overline">
+                Für Privatkunden
+                </div>
+
+                <a class="base_light" href="#">Hilfe & Kontakt</a>
+                <a class="base_light" href="#">Virtuelle Beratung</a>
+                <a class="base_light" href="#">AGBs (B2C)</a>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="footer__icons">
+
+        <div class="footer__icons-pay">
+            <img src="./assetst/icons/applepay.svg" alt="">
+            <img src="./assetst/icons/mastercard.svg" alt="">
+            <img src="./assetst/icons/creditcard.svg" alt="">
+            <img src="./assetst/icons/amex.svg" alt="">
+            <img src="./assetst/icons/klarna.svg" alt="">
+            <img src="./assetst/icons/sofort.svg" alt="">
+            <img src="./assetst/icons/visa.svg" alt="">
+            <img src="./assetst/icons/paypal.svg" alt="">
+            <span class="textXS_light">sicher bezahlen mit stripe</span>
+        </div>
+
+        <div class="footer__icons-socials">
+            <img src="./assetst/icons/social-facebook.svg" alt="">
+            <img src="./assetst/icons/social-instagram.svg" alt="">
+        </div>
+
+    </div>
+
+    <div class="footer__banner">
+        <div class="footer__banner-links base_medium">
+            <a href="#">Deutsch</a>
+            <a href="#">Cookie-Einstellungen</a>
+        </div>
+
+        <div class="footer__banner-logo">
+            <img src="./assetst/icons/Vector.svg" alt="">
+        </div>
+
+        <div class="footer__banner-certificate">
+            <img src="./assetst/icons/certificate.svg" alt="">
+        </div>
+    </div>
+
+    <div class="footer__bottom">
+
+        <div class="footer__bottom-credentials base-small_light">
+        (C) Mizzuni 2021 · Alle Rechte vorbehalten
+        </div>
+
+        <div class="footer__bottom-links">
+
+            <a class="base-small_light" href="#">AGBS für Geschäftskunden</a>
+            <a class="base-small_light" href="#">AGBS für Privatkunden</a>
+            <a class="base-small_light" href="#">Impressum</a>
+            <a class="base-small_light" href="#">Datenschutz</a>
+            <a class="base-small_light" href="#">Widerrufsbelehrung</a>
+            
         </div>
 
     </div>

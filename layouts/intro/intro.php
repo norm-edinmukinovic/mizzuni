1c228da65b7143b31ea95fbe4f94de37f3c670fd
norm-edinmukinovic <edin.mukinovic@imel.ba>
2022-02-11 07:57:13 +0100

    fix CSS

--- layouts/intro/intro.php
+++ layouts/intro/intro.php
@@ -2,13 +2,19 @@
     <img src="../../assetst/images/image.jpg" alt="">
 
 
-        <div class="intro__headline headline__large">
-        Die Eichholtz <br> Signature Collection
-        </div>
 
-        <div class="intro__button base_light">
-            Jetz entdecken <img src="../../assetst/icons/arrow.svg" alt="">
-        </div>
+        
+
+            <div class="intro__headline headline__large">
+            Die Eichholtz <br> Signature Collection
+            </div>
+
+            <div class="intro__button base_light">
+                <button> Jetz entdecken <img src="../../assetst/icons/arrow.svg" alt=""> </button> 
+            </div>
+
+
+        
 
         <div class="dashes">
             <div class="dashes__long"></div>
